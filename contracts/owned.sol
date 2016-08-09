6bb61073816e2b3d4862293a916f3d209d27df6c
Qhat Gena <qhatgena@gmail.com>
2016-08-09 12:17:47 +0700

    Add owned contract

---
+++ contracts/owned.sol
@@ -0,0 +1,14 @@
+contract Owned(){
+
+    //Address of owner
+    address Owner;
+
+    //Add modifier
+    modifier OnlyOwner{if(msg.sender == Owner)_}
+
+    //Contruction function
+    function Owned(){
+        Owner == msg.sender;
+    }
+
+}
