6ab5c2c78e3248d2d001a121af1139ffbd766703
Qhat Gena <qhatgena@gmail.com>
2016-08-09 12:18:09 +0700

    Add transfer contract

---
+++ contracts/ether-transfer.sol
@@ -0,0 +1,40 @@
+import "owned.sol";
+import "am-on-the-fork.sol";
+
+contract EtherTransfer is Owned(){
+
+//"If you are good at something, never do it for free" - Joker
+//Fee is 0.05% (it's mean you send 1 ETH fee is 0.0005 ETH)
+constant uint Fee = 5;
+constant uint Decs = 10000;
+
+//Is Vitalik Buterin on the Fork ? >_<
+AmIOnTheFork IsHeOnTheFork = AmIOnTheFork(0x2bd2326c993dfaef84f696526064ff22eba5b362);
+
+    //Only send ETH
+    function SendETH(address ETHAddress) returns(bool){
+        if(IsHeOnTheFork.forked() && ETHAddress.send(msg.value - (msg.value*Fee/Decs))){
+            return true;
+        }
+        //No ETC is trapped
+        throw;
+    }
+
+    //Only send ETC
+    function SendETC(address ETCAddress) returns(bool){
+        if(!IsHeOnTheFork.forked() && ETHAddress.send(msg.value - (msg.value*Fee/Decs))){
+            return true;
+        }
+        //No ETH is trapped
+        throw;
+    }
+
+    //I get rich lol, ez
+    function WithDraw() OnlyOwner returns(bool){
+        if(this.balance > 0 && Owner.send(this.balance)){
+            return true;
+        }
+        throw;
+    }
+
+}
